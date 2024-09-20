903e95f32512631d81aad5c811939a7e527a2f00
MelaniOtamendi <otamendimelani0@gmail.com>
2024-09-20 00:56:39 -0300

    solucion

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let btn= document.getElementById("btn");
+let button = btn.querySelector("button");
 
 btn.addEventListener("click", function() {
     alert("¡Hola! Soy el div");
+});
+
+button.addEventListener("click", function(event) {
+    event.stopPropagation();
 });
